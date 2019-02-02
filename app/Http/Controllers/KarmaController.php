0953d6ea8cd08cee008ccaa365425ab73e949511
Ashok Modi <btmash@gmail.com>
2019-02-01 19:18:08 -0800

    Add middleware stuffs

--- app/Http/Controllers/KarmaController.php
+++ app/Http/Controllers/KarmaController.php
@@ -6,7 +6,8 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Karma;
 
-class KarmaController extends Controller {
+class KarmaController extends Controller
+{
 
   /**
    * Create/update karma points for a given handle.
@@ -14,10 +15,11 @@ class KarmaController extends Controller {
    * @param  Request  $request
    * @return Response
    */
-  public function slackEvent(Request $request) {
-    if ($request->has('challenge')) {
+  public function slackEvent(Request $request)
+  {
+    if ($request->has('challenge') && $request->input('type') == 'url_verification') {
       return response()->json([
-        'challenge' => $request->input['challenge'],
+        'challenge' => $request->input('challenge'),
       ]);
     }
 
---
+++ app/Http/Middleware/KarmaMiddleware.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class KarmaMiddleware {
+
+  /**
+   * The allowed tokens.
+   *
+   * @var array
+   */
+  protected $allowed_tokens;
+
+  /**
+   * Create a new middleware instance.
+   *
+   * @return void
+   */
+  public function __construct() {
+    $this->allowed_tokens = explode('', env('ALLOWED_SLACK_TOKENS'));
+  }
+
+  /**
+   * Handle an incoming request.
+   *
+   * @param  \Illuminate\Http\Request $request
+   * @param  \Closure $next
+   *
+   * @return mixed
+   */
+  public function handle($request, Closure $next) {
+    if ($request->has('token') && in_array($request->input('token'), $this->allowed_tokens)) {
+      return $next($request);
+    }
+    return response('Unauthorized.', 401);
+  }
+
+}
