e110c19add8b03e83a9c6a58696a1b54e97dd206
Ashok Modi <btmash@gmail.com>
2019-02-02 01:20:21 -0800

    Update the karmic controller

--- app/Http/Controllers/KarmaController.php
+++ app/Http/Controllers/KarmaController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use App\Karma;
+use App\Models\Karma;
 
 class KarmaController extends Controller
 {
@@ -23,6 +23,48 @@ class KarmaController extends Controller
       ]);
     }
 
+    $message = $request->input('text');
+    $response = [];
+    if ((strpos($message, '++') !== FALSE) || (strpos($message, '--') !== FALSE)) {
+      //Yeah, an additional line. But let's not touch $message anytime
+      $subject = $message;
+      //Looks for @someone++ or @someone--
+      $pattern = '/\<\@([^\>]*)\>\:?\s?([\+\-][\+\-])/';
+      //Get all such matches
+      preg_match_all($pattern, $subject, $matches);
+      foreach ($matches[1] as $key => $handle) {
+        // @TODO: Need to figure out if user doing karma is same as
+        // handle getting karma.
+        if ($matches[2][$key] == '--') {
+          $response [] = $this->slackUpdateAndShow($handle, -1);
+        }
+        if ($matches[2][$key] == '++') {
+          $response [] = $this->slackUpdateAndShow($handle, +1);
+        }
+      }
+    }
 
+    return response([
+      'text' => implode("\n", $response),
+    ]);
   }
+
+  /**
+   * Helper function to update and output points.
+   *
+   * @param string $handle
+   *  The username to add/update.
+   * @param int $karma
+   *  The points to add/remove.
+   *
+   * @return string
+   *   The handle and the number of points that have accumulated.
+   */
+  private function slackUpdateAndShow($handle, $karma) {
+    $karma_user = Karma::firstOrNew(['handle' => $handle]);
+    $karma_user->points += $karma;
+    $karma_user->save();
+    return "<@{$handle}> now has {$karma_user->points} Karma points!";
+  }
+
 }
--- app/Models/Karma.php
+++ app/Models/Karma.php
@@ -1,17 +1,24 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Karma extends Model {
+
   /**
    * The attributes that are mass assignable.
    *
    * @var array
    */
   protected $fillable = [
-    'handle', 'karma',
+    'handle', 'points',
   ];
+
+  protected $attributes = [
+    'points' => 0,
+  ];
+
+  protected $table = 'karma';
 
 }
