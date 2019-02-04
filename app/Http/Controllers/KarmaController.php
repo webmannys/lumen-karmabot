a7b287fd79c16d9be91aa1d3d096a1fa3b757b7e
Ashok Modi <btmash@gmail.com>
2019-02-04 12:08:45 -0800

    Trying some more stuffs

--- app/Http/Controllers/KarmaController.php
+++ app/Http/Controllers/KarmaController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Jobs\ProcessSlackMention;
 use App\Jobs\ProcessSlackMessage;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -18,62 +19,20 @@ class KarmaController extends Controller {
    */
   public function queueAndRespond(Request $request) {
     $payload = $request->all();
-    dispatch(new ProcessSlackMessage($payload));
+    if (empty($payload['event'])
+      || empty($payload['event']['type'])
+      || empty($payload['event']['text'])) {
+      // Do nothing
+    }
+    elseif ($payload['event']['type'] == 'message') {
+      dispatch(new ProcessSlackMessage($payload));
+    }
+    elseif ($payload['event']['type'] == 'app_mention') {
+      dispatch(new ProcessSlackMention($payload));
+    }
     return response([
       'success' => TRUE,
     ]);
-  }
-
-  /**
-   * Create/update karma points for a given handle.
-   *
-   * @param  Request $request
-   *
-   * @return Response
-   */
-  public function slackEvent(Request $request) {
-    $message = $request->input('text');
-    $response = [];
-    if ((strpos($message, '++') !== FALSE) || (strpos($message, '--') !== FALSE)) {
-      //Yeah, an additional line. But let's not touch $message anytime
-      $subject = $message;
-      //Looks for @someone++ or @someone--
-      $pattern = '/\<\@([^\>]*)\>\:?\s?([\+\-][\+\-])/';
-      //Get all such matches
-      preg_match_all($pattern, $subject, $matches);
-      foreach ($matches[1] as $key => $handle) {
-        // @TODO: Need to figure out if user doing karma is same as
-        // handle getting karma.
-        if ($matches[2][$key] == '--') {
-          $response [] = $this->slackUpdateAndShow($handle, -1);
-        }
-        if ($matches[2][$key] == '++') {
-          $response [] = $this->slackUpdateAndShow($handle, +1);
-        }
-      }
-    }
-
-    return response([
-      'text' => implode("\n", $response),
-    ]);
-  }
-
-  /**
-   * Helper function to update and output points.
-   *
-   * @param string $handle
-   *  The username to add/update.
-   * @param int $karma
-   *  The points to add/remove.
-   *
-   * @return string
-   *   The handle and the number of points that have accumulated.
-   */
-  private function slackUpdateAndShow($handle, $karma) {
-    $karma_user = Karma::firstOrNew(['handle' => $handle]);
-    $karma_user->points += $karma;
-    $karma_user->save();
-    return "<@{$handle}> now has {$karma_user->points} Karma points!";
   }
 
 }
---
+++ app/Jobs/ProcessSlackMention.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Jobs;
+
+use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Config;
+use wrapi\slack\slack as Slack;
+
+class ProcessSlackMention extends Job {
+
+  protected $payload;
+
+  /**
+   * Create a new job instance.
+   *
+   * @param array $payload
+   *   The payload from the event.
+   *
+   * @return void
+   */
+  public function __construct(array $payload) {
+    $this->payload = $payload;
+  }
+
+  /**
+   * Execute the job.
+   *
+   * @return void
+   */
+  public function handle() {
+    $slack_client = new Slack(Config::get('services.slack.token'));
+
+    $event = $this->payload['event'];
+    $commands = [
+      'tell me a joke' => "Listen to some 'fun' jokes.",
+      'reset karma' => "This resets karma for the workspace",
+      'show me the karmas' => "This shows who has karma",
+    ];
+    $response = [];
+    if (stripos($event['text'], 'tell me a joke')) {
+      $response[] = $this->getJoke();
+    }
+
+    if (!empty($response)) {
+      $slack_client->chat->postMessage([
+        'channel' => $event['channel'],
+        'text' => implode("\n", $response),
+      ]);
+    }
+  }
+
+  /**
+   * Does a request for a joke at at the dad project.
+   *
+   * @return string
+   *   A dad joke.
+   */
+  private function sendJoke() {
+    $client = new Client();
+    $response = $client->get('https://icanhazdadjoke.com/');
+    if ($response->getStatusCode() == 200) {
+      $data = json_decode($response->getBody());
+      return $data['joke'];
+    }
+
+  }
+
+}
