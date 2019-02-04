ce712170c10d7549f05e8801cf8020c74366f26e
Ashok Modi <btmash@gmail.com>
2019-02-03 23:43:45 -0800

    Add services config

---
+++ config/services.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+  /*
+  |--------------------------------------------------------------------------
+  | Third Party Services
+  |--------------------------------------------------------------------------
+  |
+  | This file is for storing the credentials for third party services such
+  | as Stripe, Mailgun, SparkPost and others. This file provides a sane
+  | default location for this type of information, allowing packages
+  | to have a conventional place to find your various credentials.
+  |
+  */
+
+  'slack' => [
+    'token' => env('SLACK_OAUTH_ACCESS_TOKEN', ''),
+  ],
+
+];
