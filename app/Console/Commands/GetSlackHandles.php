d88857f38958e3a9b7baee1df6839eda50036ae4
Ashok Modi <btmash@gmail.com>
2019-02-04 00:59:33 -0800

    Add get slack member command

--- app/Console/Commands/GetSlackHandles.php
+++ app/Console/Commands/GetSlackHandles.php
@@ -2,41 +2,76 @@
 
 namespace App\Console\Commands;
 
+use App\Models\SlackUser;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Config;
+use wrapi\slack\slack as Slack;
 
-class GetSlackHandles extends Command
-{
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'slack:get-users';
+class GetSlackHandles extends Command {
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Retrieves all users from slack.';
+  /**
+   * The name and signature of the console command.
+   *
+   * @var string
+   */
+  protected $signature = 'slack:get-users';
 
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
+  /**
+   * The console command description.
+   *
+   * @var string
+   */
+  protected $description = 'Retrieves all users from slack.';
+
+  /**
+   * Slack client.
+   *
+   * @var \wrapi\slack\slack;
+   */
+  protected $slack_client;
+
+  /**
+   * Create a new command instance.
+   *
+   * @return void
+   */
+  public function __construct() {
+    $this->slack_client = new Slack(Config::get('services.slack.token'));
+    parent::__construct();
+  }
+
+  /**
+   * Execute the console command.
+   *
+   * @return mixed
+   */
+  public function handle() {
+    $slack_users = $this->slack_client->users->list();
+    $bar = $this->output->createProgressBar(count($slack_users));
+    $bar->start();
+    foreach ($slack_users['members'] as $slack_user) {
+      $bar->advance();
+      if ($slack_user['deleted']) {
+        $slack_member = SlackUser::where(['uid' => $slack_user['id']]);
+        if ($slack_member) {
+          $slack_member->delete();
+          continue;
+        }
+      }
+      $slack_member = SlackUser::firstOrNew([
+        'uid' => $slack_user['id'],
+      ]);
+      $slack_member->name = $slack_user['name'] ?? '';
+      $slack_member->real_name = $slack_user['real_name'] ?? '';
+      $slack_member->profile_real_name = $slack_user['profile']['real_name'] ?? '';
+      $slack_member->profile_real_name_normalized = $slack_user['profile']['real_name_normalized'] ?? '';
+      $slack_member->profile_display_name = $slack_user['profile']['display_name'] ?? '';
+      $slack_member->profile_display_name_normalized = $slack_user['profile']['display_name_normalized'] ?? '';
+      $slack_member->save();
     }
+    $bar->finish();
+    $this->output->newLine(2);
+    $this->info('Slack Member import complete!');
+  }
 
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
-    public function handle()
-    {
-        //
-    }
 }
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,25 +5,25 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Laravel\Lumen\Console\Kernel as ConsoleKernel;
 
-class Kernel extends ConsoleKernel
-{
-    /**
-     * The Artisan commands provided by your application.
-     *
-     * @var array
-     */
-    protected $commands = [
-        //
-    ];
+class Kernel extends ConsoleKernel {
 
-    /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
-    protected function schedule(Schedule $schedule)
-    {
-        //
-    }
+  /**
+   * The Artisan commands provided by your application.
+   *
+   * @var array
+   */
+  protected $commands = [
+    Commands\GetSlackHandles::class,
+  ];
+
+  /**
+   * Define the application's command schedule.
+   *
+   * @param  \Illuminate\Console\Scheduling\Schedule $schedule
+   *
+   * @return void
+   */
+  protected function schedule(Schedule $schedule) {
+    //
+  }
 }
